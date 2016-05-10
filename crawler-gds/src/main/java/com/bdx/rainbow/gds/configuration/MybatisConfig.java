189c045792955cba760ff972934c703e95a53395
mler <luyiq@163.com>
2016-05-10 08:45:29 +0800

    参数绑定

--- crawler-gds/src/main/java/com/bdx/rainbow/gds/configuration/MybatisConfig.java
+++ crawler-gds/src/main/java/com/bdx/rainbow/gds/configuration/MybatisConfig.java
@@ -25,6 +25,7 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.util.ClassUtils;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.dubbo.common.utils.StringUtils;
 
 /*
  * @see http://mybatis.github.io/spring/mappers.html
@@ -41,9 +42,57 @@ public class MybatisConfig implements EnvironmentAware{
 	@Autowired
 	private Environment environment;
 	
+	
+//	============Docker Mysql==========
+//	BSI_HZDB_PASSWORD=348be7b71b6d387
+//	BSI_HZDB_HOST=mysqlshare.servicebroker.dataos.io
+//	BSI_HZDB_URI=mysql://74e2a25af66413a:348be7b71b6d387@mysqlshare.servicebroker.dataos.io:3306/4be652d45e2d594
+//	BSI_HZDB_PORT=3306
+//	BSI_HZDB_VHOST=
+//	BSI_HZDB_NAME=4be652d45e2d594
+//	BSI_HZDB_USERNAME=74e2a25af66413a
+//	========Docker Zookeeper========
+//	BSI_HZZK_PASSWORD=28740d50b399fc17e364f2c18537176b
+//	BSI_HZZK_HOST=sb-wjmsyfulyvrlc-zk.service-brokers.svc.cluster.local
+//	BSI_HZZK_URI=
+//	BSI_HZZK_PORT=2181
+//	BSI_HZZK_VHOST=
+//	BSI_HZZK_NAME=
+//	BSI_HZZK_USERNAME=super
+	
+	/**
+	 * 如果是docker部署的话，数据库参数是从环境变量里面传递过来的，
+	 * 因此在生成datasource之前先对jdbcsetting做个预处理，将环境
+	 * 变量里面的数据库参数绑定到setting中
+	 */
+	private void preProcessJdbcConnectionSettings()
+	{
+		if(environment == null)
+			return;
+		
+		String env_db_uri = environment.getProperty("BSI_HZDB_URI");
+		if(StringUtils.isBlank(env_db_uri) ==false)
+		{
+			env_db_uri = "jdbc:"+env_db_uri+"?useUnicode=true&characterEncoding=utf8";
+			jdbcConnectionSettings.setDriver(env_db_uri);
+		}
+		
+		String env_db_username = environment.getProperty("BSI_HZDB_USERNAME");
+		if(StringUtils.isBlank(env_db_username) ==false)
+			jdbcConnectionSettings.setUsername(env_db_username);
+		
+		String env_db_password = environment.getProperty("BSI_HZDB_PASSWORD");
+		if(StringUtils.isBlank(env_db_password) ==false)
+			jdbcConnectionSettings.setPassword(env_db_password);
+		
+	}
+	
 	@Bean
 	@Primary
 	public DataSource dataSource() {
+
+		preProcessJdbcConnectionSettings();
+		
 		DruidDataSource ds = new DruidDataSource();
 		ds.setDriverClassName(jdbcConnectionSettings.getDriver());
 		ds.setUsername(jdbcConnectionSettings.getUsername());
